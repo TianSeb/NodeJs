188ed6c054a09ba296b516d394625dce3aa7294c
TianSeb <sebastianalasia@hotmail.com>
2022-10-06 16:14:06 -0300

    ejercicio servidor

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {Contenedor, Producto} from '../src/04 - FileSystem/DesafioEntregable2'
 const app = express()
 app.use(express.json())
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.get('/ping', (_,res) => {
     console.log('pinging all over the world');
@@ -15,29 +15,37 @@ app.listen(PORT, () => {
     console.log(`server is listening on ${PORT}`);
 })
 
+app.get('/productos', (_,res) => {
+    res.send(productos)
+})
+
+app.get('/productoRandom', (_,res) => {
+    res.send(randomProd(productos))
+})
+
 const path = require('path')
 const route = path.resolve(__dirname, './04 - FileSystem/Database')
 
 const contenedor = new Contenedor(route)
+let productos: string | any[] = []
+
+const randomProd = (productos:any) => {
+    const random = Math.floor(Math.random() * productos.length)
+    return productos[random]
+}
 
 const main = async (contenedor:Contenedor) => {
     try {
         console.log('Saving Products')
-        await contenedor.save(new Producto(undefined,'mayonesa',200,'ww.com'))
-        await contenedor.save(new Producto(undefined,'mostaza',400,'ww.com'))
-        await contenedor.save(new Producto(undefined,'kechu',500,'ww.com'))
-
-        console.log(await contenedor.getAll())
-        console.log(await contenedor.getById(1))
-        console.log(await contenedor.getById(22))
-
-        console.log(await contenedor.deleteById(1))
-        console.log(await contenedor.getAll())
-
-        console.log(await contenedor.deleteAll())
-        console.log(await contenedor.getAll())
+        productos = await contenedor.getAll()
+        
+        if(productos.length === 0) {
+            await contenedor.save(new Producto(undefined,'mayonesa',200,'www.elMorza.com'))
+            await contenedor.save(new Producto(undefined,'mostaza',400,'www.elMorza.com'))
+            await contenedor.save(new Producto(undefined,'kechu',500,'www.elMorza.com'))
+        }
     } catch (error) {
-        console.log('Something went wrong dude');
+        console.log('Error Loading Container || Products');
     }
 }
 
