a9de142aaa0f44c0b58de7caf84cde94ba5e50b2
TianSeb <sebastianalasia@hotmail.com>
2022-10-21 14:17:01 -0300

    ejs conditional

--- src/db/getDB.ts
+++ src/db/getDB.ts
@@ -10,9 +10,9 @@ const getDb = async () => {
         
         if(productos.length === 0) {
             console.log('Saving Products')
-            await ProductService.save(new Producto('mayonesa',200,'https://www.iconfinder.com/icons/3392469/chicken_food_fries_thigh_icon'))
-            await ProductService.save(new Producto('mostaza',400,'https://www.iconfinder.com/icons/3392483/beverages_drink_juice_orange_icon'))
-            await ProductService.save(new Producto('kechu',500,'https://www.iconfinder.com/icons/3392472/food_sussage_hot%20dog_icon'))
+            await ProductService.save(new Producto('mayonesa',200,'https://cdn3.iconfinder.com/data/icons/food-and-beverages-7/64/03_chicken_fries_thigh_food-512.png'))
+            await ProductService.save(new Producto('mostaza',400,'https://cdn3.iconfinder.com/data/icons/food-and-beverages-7/64/09_juice_drink_beverages_orange-512.png'))
+            await ProductService.save(new Producto('kechu',500,'https://cdn3.iconfinder.com/data/icons/food-and-beverages-7/64/20_hot_dog_sussage_food-512.png'))
         }
         else return ProductService
 
